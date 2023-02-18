c566809290e041d5bf15ecac11c1db47b00586fc
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-18 12:31:46 -0500

    commit tree 16c55f2dfbfda6d8470a0930ed5807f4da3ab551

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarf_array
+  dwarf_array.each.with_index(1) { |dwarf, index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls_array
+  planeteer_calls_array.map { |planeteer_call| planeteer_call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls planeteer_calls_array
+  planeteer_calls_array.any? { |planeteer_call| planeteer_call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese string_array
   cheese_types = ["cheddar", "gouda", "camembert"]
+  string_array.find { |element| element == "cheddar" || element == "gouda" || element == "camembert" }
 end
